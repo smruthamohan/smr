b3f3f48ad9031d2fe0079ee00e963b7fe7f402df
smruthamohan <144900599+smruthamohan@users.noreply.github.com>
2023-09-21 22:56:04 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+}
